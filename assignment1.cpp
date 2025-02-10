53ef87bf5d989064075f16e153afbed1c4337283
JeraldCrisBergantinos <44352027+JeraldCrisBergantinos@users.noreply.github.com>
2025-02-10 19:06:36 +0000

    Changed the variable names to snake case

--- assignment1.cpp
+++ assignment1.cpp
@@ -22,31 +22,53 @@
 #include <sstream>
 #include <iostream>
 
+// Function to reverse string.
+// Sample Input: String; 2be reversed...
+// Sample Output: gnirtS; eb2 desrever...
 std::string reverse_words(const std::string &str)
 {
-    // TODO: Implement this function
-    std::stringstream ssResult;
-    std::stringstream ssCurrentWord;
-    for (char c : str) {
-        if (isalnum(c)) ssCurrentWord << c;
+    std::stringstream result_stream;
+    std::stringstream current_word_stream;
+    for (char c : str)
+    {
+        if (isalnum(c)) // Check if the character is alphanumeric (letter or word).
+        {
+            // Alphanumeric. Append the current character.
+            current_word_stream << c;
+        }
         else {
-            std::string strWord = ssCurrentWord.str();
-            std::reverse(strWord.begin(), strWord.end());
-            ssResult << strWord << c;
-            ssCurrentWord.str("");
+            // Non-alphanumeric. Reverse the current word and append it to the result.
+            std::string current_word = current_word_stream.str();
+            std::reverse(current_word.begin(), current_word.end());
+            result_stream << current_word << c; // Append also the current character.
+            current_word_stream.str(""); // Clear the current word.
         }
     }
 
-    std::string strLastWord = ssCurrentWord.str();
-    std::reverse(strLastWord.begin(), strLastWord.end());
-    ssResult << strLastWord;
+    // Handle the last word if it exists in case
+    // the string doesn't end with a non-alphanumeric character
+    if (!current_word_stream.str().empty())
+    {
+        std::string last_word = current_word_stream.str();
+        std::reverse(last_word.begin(), last_word.end());
+        result_stream << last_word;
+    }
 
-    return ssResult.str();
+    return result_stream.str();
+}
+
+void additional_tests()
+{
+
 }
 
 int main()
 {
+    // Original test
     std::string test_str = "String; 2be reversed...";
     assert(reverse_words(test_str) == "gnirtS; eb2 desrever...");
+
+    additional_tests();
+
     return 0;
 }
